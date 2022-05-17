d83ef233ff18cea3db45197950153827cb2d100f
crombieplex <jadam310@cougarmail.cccc.edu>
2022-05-17 17:23:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const enhanceUserObject = (user, next) => {
                     }
                     user.wars = wars
                     next(null, user)
+                    })
                 })
             })
         })
