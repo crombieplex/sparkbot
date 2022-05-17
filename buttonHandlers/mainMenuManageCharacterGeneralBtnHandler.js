c1bca599c7a002c01eaf74df8f7346239d89bc57
crombieplex <jadam310@cougarmail.cccc.edu>
2022-05-17 13:05:49 -0400

    menu restructure

--- buttonHandlers/mainMenuManageCharacterGeneralBtnHandler.js
+++ buttonHandlers/mainMenuManageCharacterGeneralBtnHandler.js
@@ -2,5 +2,5 @@ const messenger = require('../lib/messenger')
 const menuFactory = require('../lib/menuFactory')
 
 module.exports = (interaction) => {
-    messenger.sendMenu(interaction.user, menuFactory.getMainCharacterMenu(interaction.user.character))
+    messenger.sendMenu(interaction.user, menuFactory.getCharacterMenu(interaction.user.character))
 }
