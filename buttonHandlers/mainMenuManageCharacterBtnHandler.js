407fd765c4293dd6550d4940a2f65b00d142f357
crombieplex <jadam310@cougarmail.cccc.edu>
2022-05-17 14:19:32 -0400

    h,mmmm

--- buttonHandlers/mainMenuManageCharacterBtnHandler.js
+++ buttonHandlers/mainMenuManageCharacterBtnHandler.js
@@ -2,8 +2,5 @@ const messenger = require('../lib/messenger')
 const menuFactory = require('../lib/menuFactory')
 
 module.exports = (interaction) => {
-  if (!interaction.user.grants.company) {
-      return;
-  }
     messenger.sendMenu(interaction.user, menuFactory.getMainCharacterMenu(interaction.user.character))
 }
