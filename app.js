66f62d7a6814cdcaf0dd16de7f407709843457e3
crombieplex <jadam310@cougarmail.cccc.edu>
2022-05-17 09:54:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const enhanceUserObject = (user, next) => {
 }
 
 client.once('ready', () => {
-    client.user.setActivity("New World", { type: "PLAYING" })
+    client.user.setActivity("Send me a DM of", { type: "HELLO" })
     logger.info('WarBot has started.')
 })
 
