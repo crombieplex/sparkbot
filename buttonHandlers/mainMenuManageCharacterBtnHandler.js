de3aa6d3722a74aca79e0e6289ae90a47bbf274c
crombieplex <jadam310@cougarmail.cccc.edu>
2022-05-17 14:08:54 -0400

    company add

--- buttonHandlers/mainMenuManageCharacterBtnHandler.js
+++ buttonHandlers/mainMenuManageCharacterBtnHandler.js
@@ -2,5 +2,8 @@ const messenger = require('../lib/messenger')
 const menuFactory = require('../lib/menuFactory')
 
 module.exports = (interaction) => {
+  if (!interaction.user.grants.company) {
+      return;
+  }
     messenger.sendMenu(interaction.user, menuFactory.getMainCharacterMenu(interaction.user.character))
 }
